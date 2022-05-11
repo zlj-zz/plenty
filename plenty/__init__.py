2d5490987978ee5c2ece8e576d302a7035f4bfea
zachary <zlj19971222@outlook.com>
2022-05-11 15:42:10 +0800

    (feat): add `Console` module.

---
+++ plenty/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+from typing import TYPE_CHECKING, Optional
+import os
+
+# For windows print color.
+if os.name == "nt":
+    os.system("")
+
+
+if TYPE_CHECKING:
+    from .console import Console
+
+
+_console: Optional["Console"] = None
+
+
+def get_console():
+    global _console
+
+    if not _console:
+        from .console import Console
+
+        _console = Console()
+
+    return _console
+
+
+def echo(*values, sep: str = " ", end: str = "\n", file=None, flush: bool = True):
+    value_list = [get_console().render_str(str(value)) for value in values]
+    print(*value_list, sep=sep, end=end, file=file, flush=flush)
